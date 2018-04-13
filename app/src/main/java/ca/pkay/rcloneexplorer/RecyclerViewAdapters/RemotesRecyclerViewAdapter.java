a3c2afa75e7f2a58a806ba58531630f169df5360
Patryk Kaczmarkiewicz <kaczmarkiewiczp@mymacewan.ca>
2018-04-13 00:27:34 -0600

    New icons library

--- app/src/main/java/ca/pkay/rcloneexplorer/RecyclerViewAdapters/RemotesRecyclerViewAdapter.java
+++ app/src/main/java/ca/pkay/rcloneexplorer/RecyclerViewAdapters/RemotesRecyclerViewAdapter.java
@@ -1,12 +1,17 @@
 package ca.pkay.rcloneexplorer.RecyclerViewAdapters;
 
 
+import android.graphics.Color;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.mikepenz.community_material_typeface_library.CommunityMaterial;
+import com.mikepenz.fontawesome_typeface_library.FontAwesome;
+import com.mikepenz.iconics.IconicsDrawable;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -21,6 +26,7 @@ public class RemotesRecyclerViewAdapter extends RecyclerView.Adapter<RemotesRecy
 
     private final List<RemoteItem> remotes;
     private final RemotesFragment.OnRemoteClickListener clickListener;
+    private View view;
 
     public RemotesRecyclerViewAdapter(List<RemoteItem> remotes, RemotesFragment.OnRemoteClickListener clickListener) {
         this.remotes = remotes;
@@ -30,7 +36,7 @@ public class RemotesRecyclerViewAdapter extends RecyclerView.Adapter<RemotesRecy
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.fragment_remotes_item, parent, false);
+        view = LayoutInflater.from(parent.getContext()).inflate(R.layout.fragment_remotes_item, parent, false);
         return new ViewHolder(view);
     }
 
@@ -43,40 +49,40 @@ public class RemotesRecyclerViewAdapter extends RecyclerView.Adapter<RemotesRecy
 
         switch (remoteType) {
             case "crypt":
-                holder.ivIcon.setImageResource(R.drawable.ic_crypt);
+                holder.ivIcon.setImageDrawable(new IconicsDrawable(view.getContext()).icon(CommunityMaterial.Icon.cmd_lock).color(Color.BLACK).sizeDp(24));
                 break;
             case "amazon cloud drive":
-                holder.ivIcon.setImageResource(R.drawable.ic_amazon_cloud_drive);
+                holder.ivIcon.setImageDrawable(new IconicsDrawable(view.getContext()).icon(CommunityMaterial.Icon.cmd_amazon).color(Color.BLACK).sizeDp(24));
                 break;
             case "b2":
-                holder.ivIcon.setImageResource(R.drawable.ic_b2);
+                holder.ivIcon.setImageDrawable(new IconicsDrawable(view.getContext()).icon(FontAwesome.Icon.faw_gripfire).color(Color.BLACK).sizeDp(24));
                 break;
             case "drive":
-                holder.ivIcon.setImageResource(R.drawable.ic_drive);
+                holder.ivIcon.setImageDrawable(new IconicsDrawable(view.getContext()).icon(CommunityMaterial.Icon.cmd_google_drive).color(Color.BLACK).sizeDp(24));
                 break;
             case "dropbox":
-                holder.ivIcon.setImageResource(R.drawable.ic_dropbox);
+                holder.ivIcon.setImageDrawable(new IconicsDrawable(view.getContext()).icon(CommunityMaterial.Icon.cmd_dropbox).color(Color.BLACK).sizeDp(24));
                 break;
             case "google cloud storage":
-                holder.ivIcon.setImageResource(R.drawable.ic_google_cloud_storage);
-                break;
-            case "swift":
-                holder.ivIcon.setImageResource(R.drawable.ic_swift);
-                break;
-            case "hubic":
-                holder.ivIcon.setImageResource(R.drawable.ic_hubic);
+                holder.ivIcon.setImageDrawable(new IconicsDrawable(view.getContext()).icon(CommunityMaterial.Icon.cmd_google_drive).color(Color.BLACK).sizeDp(24));
                 break;
             case "onedrive":
-                holder.ivIcon.setImageResource(R.drawable.ic_onedrive);
+                holder.ivIcon.setImageDrawable(new IconicsDrawable(view.getContext()).icon(CommunityMaterial.Icon.cmd_onedrive).color(Color.BLACK).sizeDp(24));
                 break;
             case "s3":
-                holder.ivIcon.setImageResource(R.drawable.ic_s3);
+                holder.ivIcon.setImageDrawable(new IconicsDrawable(view.getContext()).icon(CommunityMaterial.Icon.cmd_amazon).color(Color.BLACK).sizeDp(24));
                 break;
             case "yandex":
-                holder.ivIcon.setImageResource(R.drawable.ic_yandex);
+                holder.ivIcon.setImageDrawable(new IconicsDrawable(view.getContext()).icon(FontAwesome.Icon.faw_yandex).color(Color.BLACK).sizeDp(24));
+                break;
+            case "box":
+                holder.ivIcon.setImageDrawable(new IconicsDrawable(view.getContext()).icon(CommunityMaterial.Icon.cmd_box).color(Color.BLACK).sizeDp(24));
+                break;
+            case "sftp":
+                holder.ivIcon.setImageDrawable(new IconicsDrawable(view.getContext()).icon(FontAwesome.Icon.faw_terminal).color(Color.BLACK).sizeDp(24));
                 break;
             default:
-                    holder.ivIcon.setImageResource(R.drawable.ic_unknown);
+                holder.ivIcon.setImageDrawable(new IconicsDrawable(view.getContext()).icon(CommunityMaterial.Icon.cmd_cloud).color(Color.BLACK).sizeDp(24));
                     break;
         }
 
