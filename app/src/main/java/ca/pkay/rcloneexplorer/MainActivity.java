1a06e9dcca6fbd439ee517ded24b1a546022bb04
Patryk Kaczmarkiewicz <kaczmarkiewiczp@mymacewan.ca>
2018-04-26 00:37:45 -0600

    Update app version

--- app/src/main/java/ca/pkay/rcloneexplorer/MainActivity.java
+++ app/src/main/java/ca/pkay/rcloneexplorer/MainActivity.java
@@ -28,7 +28,6 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.InputType;
-import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
