8e9bb0aed77160690134d290791d48a9d114fa36
Patryk Kaczmarkiewicz <kaczmarkiewiczp@mymacewan.ca>
2018-04-28 22:39:09 -0600

    Add notification settings
    Option to restart app on change

--- app/src/main/java/ca/pkay/rcloneexplorer/SettingsActivity.java
+++ app/src/main/java/ca/pkay/rcloneexplorer/SettingsActivity.java
@@ -1,17 +1,23 @@
 package ca.pkay.rcloneexplorer;
 
+import android.app.Activity;
 import android.app.ActivityManager;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import ca.pkay.rcloneexplorer.Dialogs.ColorPickerDialog;
@@ -25,6 +31,7 @@ public class SettingsActivity extends AppCompatActivity {
     private View accentColorPreview;
     private Switch darkThemeSwitch;
     private View darkThemeElement;
+    private View notificationsElement;
     private Switch useLogsSwitch;
     private View useLogsElement;
     private boolean isDarkTheme;
@@ -79,6 +86,7 @@ public class SettingsActivity extends AppCompatActivity {
         accentColorPreview = findViewById(R.id.accent_color_preview);
         darkThemeSwitch = findViewById(R.id.dark_theme_switch);
         darkThemeElement = findViewById(R.id.dark_theme);
+        notificationsElement = findViewById(R.id.notifications);
         useLogsSwitch = findViewById(R.id.use_logs_switch);
         useLogsElement = findViewById(R.id.use_logs);
     }
@@ -129,6 +137,12 @@ public class SettingsActivity extends AppCompatActivity {
                 } else {
                     useLogsSwitch.setChecked(true);
                 }
+            }
+        });
+        notificationsElement.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onNotificationsClicked();
             }
         });
         useLogsSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
@@ -186,7 +200,7 @@ public class SettingsActivity extends AppCompatActivity {
         editor.apply();
 
         primaryColorPreview.getBackground().setTint(color);
-        Toasty.info(this, getString(R.string.restart_required), Toast.LENGTH_SHORT, true).show();
+        showSnackBar();
     }
 
     private void onAccentColorSelected(int color) {
@@ -196,7 +210,7 @@ public class SettingsActivity extends AppCompatActivity {
         editor.apply();
 
         accentColorPreview.getBackground().setTint(color);
-        Toasty.info(this, getString(R.string.restart_required), Toast.LENGTH_SHORT, true).show();
+        showSnackBar();
     }
 
     private void onDarkThemeClicked(boolean isChecked) {
@@ -205,7 +219,21 @@ public class SettingsActivity extends AppCompatActivity {
         editor.putBoolean(getString(R.string.pref_key_dark_theme), isChecked);
         editor.apply();
 
-        Toasty.info(this, getString(R.string.restart_required), Toast.LENGTH_SHORT, true).show();
+        showSnackBar();
+    }
+
+    private void onNotificationsClicked() {
+        Intent intent = new Intent();
+        intent.setAction("android.settings.APP_NOTIFICATION_SETTINGS");
+
+        //for Android 5-7
+        intent.putExtra("app_package", getPackageName());
+        intent.putExtra("app_uid", getApplicationInfo().uid);
+
+        // for Android O
+        intent.putExtra("android.provider.extra.APP_PACKAGE", getPackageName());
+
+        startActivity(intent);
     }
 
     private void onUseLogsClicked(boolean isChecked) {
@@ -213,5 +241,28 @@ public class SettingsActivity extends AppCompatActivity {
         SharedPreferences.Editor editor = sharedPreferences.edit();
         editor.putBoolean(getString(R.string.pref_key_logs), isChecked);
         editor.apply();
+    }
+
+    private void showSnackBar() {
+        Snackbar snackbar = Snackbar.make(findViewById(R.id.cl_activity_settings), R.string.restart_required, Snackbar.LENGTH_LONG);
+        if (isDarkTheme) {
+            snackbar.getView().setBackgroundColor(getResources().getColor(R.color.white));
+            TextView tv = snackbar.getView().findViewById(android.support.design.R.id.snackbar_text);
+            tv.setTextColor(getResources().getColor(android.R.color.black));
+        }
+        snackbar.setAction("Restart", new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                restartApp();
+            }
+        });
+        snackbar.show();
+    }
+
+    private void restartApp() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+        Runtime.getRuntime().exit(0);
     }
 }
