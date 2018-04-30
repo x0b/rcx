1a34a66cecc7a923cc3c6c9847d94b3a08cda603
Patryk Kaczmarkiewicz <kaczmarkiewiczp@mymacewan.ca>
2018-04-29 22:27:59 -0600

    Fix sorting while directory loading

--- app/src/main/java/ca/pkay/rcloneexplorer/Fragments/FileExplorerFragment.java
+++ app/src/main/java/ca/pkay/rcloneexplorer/Fragments/FileExplorerFragment.java
@@ -440,7 +440,9 @@ public class FileExplorerFragment extends Fragment implements   FileExplorerRecy
                 .setListener(new SortDialog.OnClickListener() {
                     @Override
                     public void onPositiveButtonClick(int sortById, int sortOrderId) {
-                        sortSelected(sortById, sortOrderId);
+                        if (directoryContent != null && !directoryContent.isEmpty()) {
+                            sortSelected(sortById, sortOrderId);
+                        }
                     }
                 })
                 .setSortOrder(sortOrder)
@@ -590,6 +592,7 @@ public class FileExplorerFragment extends Fragment implements   FileExplorerRecy
         }
         path = fileItem.getPath();
         recyclerViewAdapter.clear();
+        directoryContent.clear();
         fetchDirectoryTask = new FetchDirectoryContent().execute();
 
     }
