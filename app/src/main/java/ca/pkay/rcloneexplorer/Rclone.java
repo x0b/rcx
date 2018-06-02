e5d6db3fdefa5243c8d08cd1abf36a882705abdd
Patryk Kaczmarkiewicz <kaczmarkiewiczp@mymacewan.ca>
2018-06-02 15:47:17 -0600

    Fix possible crash when loading remotes

--- app/src/main/java/ca/pkay/rcloneexplorer/Rclone.java
+++ app/src/main/java/ca/pkay/rcloneexplorer/Rclone.java
@@ -194,7 +194,7 @@ public class Rclone {
             try {
                 JSONObject remoteJSON = new JSONObject(remotesJSON.get(key).toString());
                 String type = remoteJSON.getString("type");
-                if (remoteJSON.has("remote")) {
+                if (remoteJSON.has("remote") && !remoteJSON.getString("remote").isEmpty()) {
                     String remotePath = remoteJSON.getString("remote");
                     int index = remotePath.indexOf(":");
                     RemoteItem remote = getRemote(remotesJSON, remotePath.substring(0, index));
