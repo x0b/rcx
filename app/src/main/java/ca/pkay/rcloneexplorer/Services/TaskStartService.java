4a65e89f0a05fe644762f395b529b83bace40290
Felix Nüsse <felix.nuesse@t-online.de>
2019-12-22 13:38:46 +0100

    fix service not starting from background

--- app/src/main/java/ca/pkay/rcloneexplorer/Services/TaskStartService.java
+++ app/src/main/java/ca/pkay/rcloneexplorer/Services/TaskStartService.java
@@ -1,8 +1,13 @@
 package ca.pkay.rcloneexplorer.Services;
 
 import android.app.IntentService;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Intent;
 import android.content.Context;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import ca.pkay.rcloneexplorer.Database.DatabaseHandler;
@@ -23,11 +28,29 @@ public class TaskStartService extends IntentService {
         Log.e("Service", "Start service intent!");
     }
 
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        if (Build.VERSION.SDK_INT >= 26) {
+            String CHANNEL_ID = "my_channel_01";
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID,
+                    "Channel human readable title",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+
+            ((NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE)).createNotificationChannel(channel);
+
+            Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID)
+                    .setContentTitle("")
+                    .setContentText("").build();
+
+            startForeground(1, notification);
+        }
+    }
 
     @Override
     protected void onHandleIntent(Intent intent) {
        // Log.e("Service", "Start Intent: "+intent.);
-
         if (intent != null) {
             final String action = intent.getAction();
             if (action.equals("START_TASK")) {
@@ -46,7 +69,11 @@ public class TaskStartService extends IntentService {
                         i.putExtra(SyncService.LOCAL_PATH_ARG, path);
                         i.putExtra(SyncService.SYNC_DIRECTION_ARG, t.getDirection());
                         i.putExtra(SyncService.REMOTE_PATH_ARG, t.getRemote_path());
-                        startService(i);
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                            startForegroundService(i);
+                        }else {
+                            startService(i);
+                        }
                     }
                 }
             }
