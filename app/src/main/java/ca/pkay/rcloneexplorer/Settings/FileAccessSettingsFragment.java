074b839d1876c0a14ca153f874098fab177f565d
x0b <x0bdev@gmail.com>
2020-03-06 09:26:48 +0100

    Add Android R MANAGE_EXTERNAL_STORAGE permission
    Android R (11) DP1 introduces MANAGE_EXTERNAL_STORAGE and disables SAF tree
    root access. This commit adds a button to the settings pane to allow Android R
    Preview users to grant the app this permission.

--- app/src/main/java/ca/pkay/rcloneexplorer/Settings/FileAccessSettingsFragment.java
+++ app/src/main/java/ca/pkay/rcloneexplorer/Settings/FileAccessSettingsFragment.java
@@ -21,6 +21,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.preference.PreferenceManager;
 import androidx.recyclerview.widget.RecyclerView;
+import ca.pkay.rcloneexplorer.BuildConfig;
 import ca.pkay.rcloneexplorer.R;
 import ca.pkay.rcloneexplorer.Rclone;
 import ca.pkay.rcloneexplorer.RemoteConfig.RemoteConfigHelper;
@@ -31,9 +32,12 @@ import io.github.x0b.safdav.file.SafConstants;
 import java.util.ArrayList;
 import java.util.List;
 
+import static ca.pkay.rcloneexplorer.ActivityHelper.tryStartActivityForResult;
+
 public class FileAccessSettingsFragment extends Fragment {
 
     public static final int onDocumentTreeOpened = 1001;
+    private static final int onAllFilesSettingOpened = 1002;
 
     private Context context;
     private ViewGroup fileAccessAll;
@@ -45,6 +49,7 @@ public class FileAccessSettingsFragment extends Fragment {
     private View addButtonContainer;
     private View refreshLaContainer;
     private Switch refreshLaSwitch;
+    private View openAllFilesPerm;
     private Rclone rclone;
 
     public static FileAccessSettingsFragment newInstance() {
@@ -97,6 +102,7 @@ public class FileAccessSettingsFragment extends Fragment {
         addPermissionBtn = view.findViewById(R.id.permission_add_button);
         refreshLaContainer = view.findViewById(R.id.enable_refresh_la_container);
         refreshLaSwitch = view.findViewById(R.id.enable_refresh_la_switch);
+        openAllFilesPerm = view.findViewById(R.id.open_all_files_setting_container);
     }
 
     private void setDefaultStates() {
@@ -113,10 +119,11 @@ public class FileAccessSettingsFragment extends Fragment {
         addPermissionBtn.setOnClickListener(v -> addRoot());
         refreshLaContainer.setOnClickListener(v -> refreshLaSwitch.setChecked(!refreshLaSwitch.isChecked()));
         refreshLaSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> setRefreshLa(isChecked));
+        openAllFilesPerm.setOnClickListener(v -> openAndroidRAllFilesSettings());
     }
 
     private void setSafEnabled(boolean isChecked) {
-        if(isChecked) {
+        if (isChecked) {
             listView.setVisibility(View.VISIBLE);
             addButtonContainer.setVisibility(View.VISIBLE);
             createSafRemote();
@@ -136,11 +143,18 @@ public class FileAccessSettingsFragment extends Fragment {
         pref.edit().putBoolean(getString(R.string.pref_key_refresh_local_aliases), isChecked).apply();
     }
 
+    private void openAndroidRAllFilesSettings() {
+        // TODO @CompileSDK 30: Migrate to framework constants
+        Intent intent = new Intent("android.settings.MANAGE_APP_ALL_FILES_ACCESS_PERMISSION");
+        intent.setData(Uri.fromParts("package", BuildConfig.APPLICATION_ID, null));
+        tryStartActivityForResult(this, intent, onAllFilesSettingOpened);
+    }
+
     private void createSafRemote() {
         RemoteConfigHelper.enableSaf(getContext());
     }
 
-    public void addRoot(){
+    public void addRoot() {
         Intent treeOpenIntent = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE);
         treeOpenIntent.addFlags(Intent.FLAG_GRANT_PREFIX_URI_PERMISSION | Intent.FLAG_GRANT_PERSISTABLE_URI_PERMISSION);
         startActivityForResult(treeOpenIntent, onDocumentTreeOpened);
@@ -148,39 +162,51 @@ public class FileAccessSettingsFragment extends Fragment {
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if(Activity.RESULT_OK == resultCode) {
+        switch (requestCode) {
+            case onDocumentTreeOpened:
+                onTreeResult(resultCode, data);
+                break;
+            case onAllFilesSettingOpened:
+                Toasty.info(context, "All file access beta callback", Toast.LENGTH_LONG, true).show();
+                break;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+
+
+    private void onTreeResult(int resultCode, Intent data) {
+        if (Activity.RESULT_OK == resultCode) {
             Uri uri = data.getData();
-            if (null == uri || uri.getAuthority().equals("io.github.x0b.safdav")){
+            if (null == uri || uri.getAuthority().equals("io.github.x0b.safdav")) {
                 Toasty.error(context, getString(R.string.saf_uri_permission_error), Toast.LENGTH_LONG, true).show();
                 return;
             }
-            if (requestCode == onDocumentTreeOpened) {
-                final int takeFlags = data.getFlags()
-                        & (Intent.FLAG_GRANT_READ_URI_PERMISSION
-                        | Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
-                ContentResolver contentResolver = context.getContentResolver();
-                for (UriPermission uriPermission : contentResolver.getPersistedUriPermissions()) {
-                    if(uri.toString().startsWith(uriPermission.getUri().toString())){
-                        // granted uri is less specific than already granted ones, reject
-                        Toasty.error(context, getString(R.string.saf_uri_permission_already_added), Toast.LENGTH_LONG, true).show();
-                        return;
-                    }
-                }
-                // discard non-android storage devices
-                if(!"com.android.externalstorage.documents".equals(uri.getAuthority())){
-                    Toasty.error(context, getString(R.string.saf_uri_permission_no_support), Toast.LENGTH_LONG, true).show();
+            final int takeFlags = data.getFlags()
+                    & (Intent.FLAG_GRANT_READ_URI_PERMISSION | Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+            ContentResolver contentResolver = context.getContentResolver();
+            for (UriPermission uriPermission : contentResolver.getPersistedUriPermissions()) {
+                if (uri.toString().startsWith(uriPermission.getUri().toString())) {
+                    // granted uri is less specific than already granted ones, reject
+                    Toasty.error(context, getString(R.string.saf_uri_permission_already_added), Toast.LENGTH_LONG, true).show();
                     return;
                 }
-                contentResolver.takePersistableUriPermission(uri, takeFlags);
-                permissionList.updatePermissions(context.getContentResolver().getPersistedUriPermissions());
-                Toasty.normal(context, getString(R.string.saf_uri_permission_added), Toast.LENGTH_SHORT).show();
             }
-        } else if(Activity.RESULT_CANCELED == resultCode){
+            // discard non-android storage devices
+            if (!"com.android.externalstorage.documents".equals(uri.getAuthority())) {
+                Toasty.error(context, getString(R.string.saf_uri_permission_no_support), Toast.LENGTH_LONG, true).show();
+                return;
+            }
+            contentResolver.takePersistableUriPermission(uri, takeFlags);
+            permissionList.updatePermissions(context.getContentResolver().getPersistedUriPermissions());
+            Toasty.normal(context, getString(R.string.saf_uri_permission_added), Toast.LENGTH_SHORT).show();
+
+        } else if (Activity.RESULT_CANCELED == resultCode) {
             Toasty.normal(context, getString(R.string.saf_uri_permission_cancelled), Toast.LENGTH_SHORT).show();
-        }
-        super.onActivityResult(requestCode, resultCode, data);
-        if (getActivity() != null) {
-            getActivity().finish();
         }
     }
 
@@ -189,7 +215,7 @@ public class FileAccessSettingsFragment extends Fragment {
         TextView text;
         UriPermission permission;
 
-        public PermissionsViewHolder(@NonNull View itemView, final PermissionListAdapter adapter){
+        public PermissionsViewHolder(@NonNull View itemView, final PermissionListAdapter adapter) {
             super(itemView);
             text = itemView.findViewById(R.id.permission_path);
             itemView.findViewById(R.id.permission_remove).setOnClickListener(v -> adapter.remove(getAdapterPosition()));
@@ -215,7 +241,7 @@ public class FileAccessSettingsFragment extends Fragment {
         public void onBindViewHolder(@NonNull PermissionsViewHolder holder, int position) {
             holder.permission = permissions.get(position);
             String permissionLabel = holder.permission.getUri().getPath();
-            if("/tree/primary:".equals(permissionLabel)) {
+            if ("/tree/primary:".equals(permissionLabel)) {
                 permissionLabel = getString(R.string.pref_saf_permission_label_primary, permissionLabel);
             } else {
                 permissionLabel = getString(R.string.pref_saf_permission_label_external, permissionLabel);
@@ -228,12 +254,12 @@ public class FileAccessSettingsFragment extends Fragment {
             return permissions.size();
         }
 
-        public void add(UriPermission permission){
+        public void add(UriPermission permission) {
             permissions.add(permission);
             notifyDataSetChanged();
         }
 
-        public void remove(int index){
+        public void remove(int index) {
             UriPermission permission = permissions.get(index);
             context.getContentResolver().releasePersistableUriPermission(permission.getUri(),
                     Intent.FLAG_GRANT_READ_URI_PERMISSION | Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
