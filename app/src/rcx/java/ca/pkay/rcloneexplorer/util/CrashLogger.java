81582e0f0007afef3e35fe84e264a00c039fc184
x0b <x0bdev@gmail.com>
2020-04-06 13:28:11 +0200

    [rcx] Repair crash logging and replaced Fabric with Firebase

--- app/src/rcx/java/ca/pkay/rcloneexplorer/util/CrashLogger.java
+++ app/src/rcx/java/ca/pkay/rcloneexplorer/util/CrashLogger.java
@@ -1,12 +1,12 @@
 package ca.pkay.rcloneexplorer.util;
 
 import android.content.Context;
-import com.crashlytics.android.Crashlytics;
-import io.fabric.sdk.android.Fabric;
+import com.google.firebase.crashlytics.FirebaseCrashlytics;
 
 public class CrashLogger {
 
     public static void initCrashLogging(Context context) {
-        Fabric.with(context, new Crashlytics());
+        FirebaseCrashlytics crashlytics = FirebaseCrashlytics.getInstance();
+        crashlytics.setCrashlyticsCollectionEnabled(true);
     }
 }
