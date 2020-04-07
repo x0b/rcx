x0b/rcx

commit a3d428b9328cca9eee0ad8eb54911e5d5c9775fa

Fix: Crash after granting storage permission
Ref: IllegalStateException @ MainActivity.java:778

app/src/main/java/ca/pkay/rcloneexplorer/MainActivity.java
@@ -768,6 +768,11 @@ public class MainActivity extends AppCompatActivity
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
+            if(isFinishing() || isDestroyed()) {
+                FLog.w(TAG, "Invalid state, stopping drive refresh");
+                cancel(true);
+                return;
+            }
             View v = findViewById(R.id.locked_config);
             if (v != null) {
                 v.setVisibility(View.GONE);
@@ -780,6 +785,9 @@ public class MainActivity extends AppCompatActivity
 
         @Override
         protected Boolean doInBackground(Void... aVoid) {
+            if (isCancelled()) {
+                return null;
+            }
             SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(context);
             Set<String> generated = pref.getStringSet(getString(R.string.pref_key_local_alias_remotes), new HashSet<>());
             Set<String> renamed = pref.getStringSet(getString(R.string.pref_key_renamed_remotes), new HashSet<>());

commit c911c2feea3bda321887e85eacbabaa25a3e195b

Fix: Crash after granting storage permission, part II
Refine protection introduced in a3d428b9328cca9eee0ad8eb54911e5d5c9775fa for
better stability.

Ref: IllegalStateException @ MainActivity.java:783

app/src/main/java/ca/pkay/rcloneexplorer/MainActivity.java
@@ -768,7 +768,7 @@ public class MainActivity extends AppCompatActivity
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
-            if(isFinishing() || isDestroyed()) {
+            if(isFinishing() || isDestroyed() || getSupportFragmentManager().isStateSaved()) {
                 FLog.w(TAG, "Invalid state, stopping drive refresh");
                 cancel(true);
                 return;