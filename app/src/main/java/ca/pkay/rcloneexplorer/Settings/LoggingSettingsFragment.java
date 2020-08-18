b0569d9cbd45d052f7c97a4edd9aa89f8f5baf47
x0b <x0bdev@gmail.com>
2020-08-18 19:01:46 +0200

    Crash Logging: Add dummy report button to settings

--- app/src/main/java/ca/pkay/rcloneexplorer/Settings/LoggingSettingsFragment.java
+++ app/src/main/java/ca/pkay/rcloneexplorer/Settings/LoggingSettingsFragment.java
@@ -10,11 +10,13 @@ import androidx.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.Toast;
 
+import ca.pkay.rcloneexplorer.BuildConfig;
 import ca.pkay.rcloneexplorer.R;
+import ca.pkay.rcloneexplorer.util.CrashLogger;
+import ca.pkay.rcloneexplorer.util.FLog;
 import es.dmoral.toasty.Toasty;
 
 public class LoggingSettingsFragment extends Fragment {
@@ -24,6 +26,7 @@ public class LoggingSettingsFragment extends Fragment {
     private View useLogsElement;
     private View crashReportsElement;
     private Switch crashReportsSwitch;
+    private View testReportElement;
 
     /**
      * Mandatory empty constructor for the fragment manager to instantiate the
@@ -67,34 +70,30 @@ public class LoggingSettingsFragment extends Fragment {
         useLogsElement = view.findViewById(R.id.use_logs);
         crashReportsElement = view.findViewById(R.id.crash_reporting);
         crashReportsSwitch = view.findViewById(R.id.crash_reporting_switch);
+        testReportElement = view.findViewById(R.id.send_test_report);
     }
 
     private void setDefaultStates() {
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
         boolean useLogs = sharedPreferences.getBoolean(getString(R.string.pref_key_logs), false);
-        boolean crashReports = sharedPreferences.getBoolean(getString(R.string.pref_key_crash_reports), false);
+        boolean crashReports = sharedPreferences.getBoolean(getString(R.string.pref_key_crash_reports),
+                getResources().getBoolean(R.bool.default_crash_log_enable));
 
         useLogsSwitch.setChecked(useLogs);
         crashReportsSwitch.setChecked(crashReports);
     }
 
     private void setClickListeners() {
-        useLogsElement.setOnClickListener(v -> {
-            if (useLogsSwitch.isChecked()) {
-                useLogsSwitch.setChecked(false);
-            } else {
-                useLogsSwitch.setChecked(true);
-            }
-        });
+        useLogsElement.setOnClickListener(v -> useLogsSwitch.setChecked(!useLogsSwitch.isChecked()));
         useLogsSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> onUseLogsClicked(isChecked));
-        crashReportsElement.setOnClickListener(v -> {
-            if (crashReportsSwitch.isChecked()) {
-                crashReportsSwitch.setChecked(false);
-            } else {
-                crashReportsSwitch.setChecked(true);
-            }
-        });
+        crashReportsElement.setOnClickListener(v -> crashReportsSwitch.setChecked(!crashReportsSwitch.isChecked()));
         crashReportsSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> crashReportsClicked(isChecked));
+        testReportElement.setOnClickListener(v -> FLog.e(
+                "TestReport",
+                "Sending test report, %s, %s, %s",
+                "/storage/0/private.file",
+                "content://authority/private.file",
+                "Non-filterd argument"));
     }
 
     private void onUseLogsClicked(boolean isChecked) {
