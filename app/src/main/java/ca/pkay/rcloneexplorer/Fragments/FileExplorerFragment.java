x0b/rcx

commit 887084128bafbabe1cecd65bb8a72a7791d56d0c

Fix #36: VLC can't play audio files.
Details: https://code.videolan.org/videolan/vlc-android/issues/1151

app/src/main/java/ca/pkay/rcloneexplorer/Fragments/FileExplorerFragment.java
@@ -1,6 +1,7 @@
 package ca.pkay.rcloneexplorer.Fragments;
 
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.ClipData;
 import android.content.ClipboardManager;
@@ -1974,8 +1975,7 @@ public class FileExplorerFragment extends Fragment implements   FileExplorerRecy
             boolean available = false;
             while (retries > 0) {
                 try {
-                    URL checkUrl = new URL(uri.toString());
-                    FLog.v(TAG, "doInBackground: GET %s", checkUrl.toString());
+                    FLog.v(TAG, "doInBackground: HEAD %s", uri.toString());
                     Response response = client.newCall(request).execute();
                     code = response.code();
 
@@ -2021,7 +2021,11 @@ public class FileExplorerFragment extends Fragment implements   FileExplorerRecy
             }
             Dialogs.dismissSilently(loadingDialog);
             if(success) {
-                tryStartActivityForResult(FileExplorerFragment.this, intent, STREAMING_INTENT_RESULT);
+                Activity activity = getActivity();
+                if (null == activity) {
+                    return;
+                }
+                tryStartActivityForResult(activity, intent, STREAMING_INTENT_RESULT);
             } else {
                 Toasty.error(context, getString(R.string.streaming_task_failed), Toast.LENGTH_LONG, true).show();
                 context.stopService(serveIntent);

commit caa5b6442e1fe79b4bee1ddccfc57bceb1fab269

Streaming: Fix opening a second file fails to stream
Fixes regression introduced in
887084128bafbabe1cecd65bb8a72a7791d56d0c that caused the result callback to be
routed to the parent activity, leaving the service running.

Closes #87.

app/src/main/java/ca/pkay/rcloneexplorer/Fragments/FileExplorerFragment.java
@@ -108,13 +108,13 @@ public class FileExplorerFragment extends Fragment implements   FileExplorerRecy
                                                                 SortDialog.OnClickListener,
                                                                 ServeDialog.Callback {
 
+    public static final int STREAMING_INTENT_RESULT = 468;
     private static final String TAG = "FileExplorerFragment";
     private static final String ARG_REMOTE = "remote_param";
     private static final String SHARED_PREFS_SORT_ORDER = "ca.pkay.rcexplorer.sort_order";
     private static final int FILE_PICKER_UPLOAD_RESULT = 186;
     private static final int FILE_PICKER_DOWNLOAD_RESULT = 204;
     private static final int FILE_PICKER_SYNC_RESULT = 45;
-    private static final int STREAMING_INTENT_RESULT = 468;
     private final String SAVED_PATH = "ca.pkay.rcexplorer.FILE_EXPLORER_FRAG_SAVED_PATH";
     private final String SAVED_CONTENT = "ca.pkay.rcexplorer.FILE_EXPLORER_FRAG_SAVED_CONTENT";
     private final String SAVED_SEARCH_MODE = "ca.pkay.rcexplorer.FILE_EXPLORER_FRAG_SEARCH_MODE";
@@ -666,6 +666,9 @@ public class FileExplorerFragment extends Fragment implements   FileExplorerRecy
     // serve callback
     @Override
     public void onServeOptionsSelected(int protocol, boolean allowRemoteAccess, String user, String password) {
+        // GH-87: Release old stream
+        context.stopService(new Intent(context, StreamingService.class));
+
         Intent intent = new Intent(getContext(), StreamingService.class);
         intent.putExtra(StreamingService.SERVE_PATH_ARG, directoryObject.getCurrentPath());
         intent.putExtra(StreamingService.REMOTE_ARG, remote);
@@ -1319,6 +1322,8 @@ public class FileExplorerFragment extends Fragment implements   FileExplorerRecy
                             default:
                                 return;
                         }
+                        // GH-87: Release old server
+                        context.stopService(new Intent(context, StreamingService.class));
                         tryStartService(context, intent);
                     });
                     builder.setTitle(R.string.pick_a_protocol);
@@ -1948,6 +1953,8 @@ public class FileExplorerFragment extends Fragment implements   FileExplorerRecy
             serveIntent.putExtra(StreamingService.REMOTE_ARG, remote);
             serveIntent.putExtra(StreamingService.SHOW_NOTIFICATION_TEXT, false);
             serveIntent.putExtra(StreamingService.SERVE_PORT, port);
+            // GH-87: Release old stream
+            context.stopService(new Intent(context, StreamingService.class));
             tryStartService(context, serveIntent);
 
             Uri uri = Uri.parse("http://127.0.0.1:" + port)

app/src/main/java/ca/pkay/rcloneexplorer/MainActivity.java
@@ -46,6 +46,7 @@ import ca.pkay.rcloneexplorer.Fragments.FileExplorerFragment;
 import ca.pkay.rcloneexplorer.Fragments.RemotesFragment;
 import ca.pkay.rcloneexplorer.Items.RemoteItem;
 import ca.pkay.rcloneexplorer.RemoteConfig.RemoteConfigHelper;
+import ca.pkay.rcloneexplorer.Services.StreamingService;
 import ca.pkay.rcloneexplorer.Settings.SettingsActivity;
 import ca.pkay.rcloneexplorer.util.CrashLogger;
 import ca.pkay.rcloneexplorer.util.FLog;
@@ -287,6 +288,9 @@ public class MainActivity extends AppCompatActivity
             if(refresh.isRequired()) {
                 refresh.execute();
             }
+        } else if (requestCode == FileExplorerFragment.STREAMING_INTENT_RESULT) {
+            Intent serveIntent = new Intent(this, StreamingService.class);
+            context.stopService(serveIntent);
         }
     }
 