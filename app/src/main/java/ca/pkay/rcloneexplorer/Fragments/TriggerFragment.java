d3545a7fa107c4252419d3c92e7bc9199cc19e45
Felix Nüsse <felix.nuesse@t-online.de>
2021-06-13 11:01:01 +0200

    Show errorscreen when no tasks are defined to prevent userconfusion

--- app/src/main/java/ca/pkay/rcloneexplorer/Fragments/TriggerFragment.java
+++ app/src/main/java/ca/pkay/rcloneexplorer/Fragments/TriggerFragment.java
@@ -12,14 +12,18 @@ import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import ca.pkay.rcloneexplorer.Database.DatabaseHandler;
 import ca.pkay.rcloneexplorer.Database.json.Exporter;
 import ca.pkay.rcloneexplorer.Database.json.Importer;
+import ca.pkay.rcloneexplorer.Items.Trigger;
 import ca.pkay.rcloneexplorer.R;
 import ca.pkay.rcloneexplorer.RecyclerViewAdapters.TriggerRecyclerViewAdapter;
+import ca.pkay.rcloneexplorer.Services.TriggerService;
 import ca.pkay.rcloneexplorer.TriggerActivity;
 import jp.wasabeef.recyclerview.animators.LandingAnimator;
 
@@ -27,6 +31,7 @@ public class TriggerFragment extends Fragment {
 
     private TriggerRecyclerViewAdapter recyclerViewAdapter;
     private View fragmentView;
+    private DatabaseHandler dbHandler;
 
 
     public TriggerFragment() {
@@ -49,11 +54,32 @@ public class TriggerFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_trigger, container, false);
         fragmentView = view;
+
         populateTriggerList(fragmentView);
+
+        if(dbHandler.getAllTasks().size() > 0 ){
+            view.findViewById(R.id.layout_error).setVisibility(View.GONE);
+            view.findViewById(R.id.layout_list).setVisibility(View.VISIBLE);
+        } else {
+            //disable all trigger when no tasks are available.
+            TriggerService triggerService = new TriggerService(view.getContext());
+            for(Trigger trigger : dbHandler.getAllTrigger()){
+                triggerService.cancelTrigger(trigger.getId());
+            }
+        }
 
         view.findViewById(R.id.newTrigger).setOnClickListener(v -> {
             Intent intent = new Intent(view.getContext(), TriggerActivity.class);
             startActivity(intent);
+        });
+
+        view.findViewById(R.id.task_activity_button).setOnClickListener(v -> {
+            Fragment fragment = new TasksFragment();
+
+            FragmentManager fm = ((FragmentActivity) getContext()).getSupportFragmentManager();
+            FragmentTransaction transaction = fm.beginTransaction();
+            transaction.replace(R.id.flFragment, fragment);
+            transaction.commit();
         });
 
         return view;
@@ -78,8 +104,9 @@ public class TriggerFragment extends Fragment {
 
     private void populateTriggerList(View v){
         Context c = v.getContext();
-        DatabaseHandler dbHandler = new DatabaseHandler(c);
-
+        if(dbHandler == null){
+            dbHandler = new DatabaseHandler(c);
+        }
         RecyclerView recyclerView =  v.findViewById(R.id.trigger_list);
         recyclerView.setLayoutManager(new LinearLayoutManager(c));
         recyclerView.setItemAnimator(new LandingAnimator());
